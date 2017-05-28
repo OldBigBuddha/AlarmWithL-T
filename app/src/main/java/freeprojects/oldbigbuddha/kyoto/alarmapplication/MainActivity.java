cea731da6101bbbe4e0d4e0cd72d6721a828228a
Developer <developer@localhost.localdomain>
2017-05-28 23:31:39 +0900

    [change]castomSetter

--- app/src/main/java/freeprojects/oldbigbuddha/kyoto/alarmapplication/MainActivity.java
+++ app/src/main/java/freeprojects/oldbigbuddha/kyoto/alarmapplication/MainActivity.java
@@ -1,10 +1,12 @@
 package freeprojects.oldbigbuddha.kyoto.alarmapplication;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
 import android.databinding.adapters.TextViewBindingAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.Spanned;
 import android.widget.TextView;
 
 import freeprojects.oldbigbuddha.kyoto.alarmapplication.databinding.ActivityMainBinding;
@@ -18,7 +20,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView( this, R.layout.activity_main );
-        TextView textView = (TextView)findViewById(R.id.tvHome);
-        textView.setText(Html.fromHtml( getString(R.string.title_home) ) );
+    }
+
+    @BindingAdapter("htmlText")
+    public static void setText(TextView view, String text) {
+        view.setText( fromHtml( text ) );
+    }
+
+    @SuppressWarnings("deprecation")
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            result = Html.fromHtml(html,Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            result = Html.fromHtml(html);
+        }
+        return result;
     }
 }
