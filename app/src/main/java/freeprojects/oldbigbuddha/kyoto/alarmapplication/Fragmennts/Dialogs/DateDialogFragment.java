edf070e5631d269ea996d51f265e202c9820859b
Big Buddha <shinlag@outlook.jp>
2017-08-02 08:52:45 +0900

    [add]Japanese

---
+++ app/src/main/java/freeprojects/oldbigbuddha/kyoto/alarmapplication/Fragmennts/Dialogs/DateDialogFragment.java
@@ -0,0 +1,13 @@
+package freeprojects.oldbigbuddha.kyoto.alarmapplication.Fragmennts.Dialogs;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by developer on 17/08/02.
+ */
+
+public class DateDialogFragment extends DialogFragment {
+
+    public DateDialogFragment() {}
+
+}
